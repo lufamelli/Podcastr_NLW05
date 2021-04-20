1af85285867bda05a4eca2f37f1e1959f634c5c9
Lucas Famelli <lucasfamelli@hotmail.com>
2021-04-20 18:25:21 -0300

    add Header and Player components, add date-fns library

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1>Hello world!</h1>;
+// SPA
+// SSR
+// SSG
+
+export default function Home(props) {
+  return <div> </div>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
